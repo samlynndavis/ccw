2ecffb236d8ff7bec0e54dc4ecb73f133539e51d
Samuel Davis <hello@smdvs.com>
2019-12-24 17:47:04 -0600

    Roster panel fixes and general responsiveness tweaks

--- scripts.js
+++ scripts.js
@@ -134,36 +134,36 @@ grabber.addEventListener('mouseup', () => {
   grabber.classList.remove('active');
 });
 
-const slider = document.querySelector('.roster');
-let isDown = false;
-let startX;
-let scrollLeft;
+// const slider = document.querySelector('.roster');
+// let isDown = false;
+// let startX;
+// let scrollLeft;
 
-slider.addEventListener('mousedown', (e) => {
-  isDown = true;
-  slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
+// slider.addEventListener('mousedown', (e) => {
+//   isDown = true;
+//   slider.classList.add('active');
+//   startX = e.pageX - slider.offsetLeft;
+//   scrollLeft = slider.scrollLeft;
+// });
 
-slider.addEventListener('mouseleave', () => {
-  isDown = false;
-  slider.classList.remove('active');
-});
+// slider.addEventListener('mouseleave', () => {
+//   isDown = false;
+//   slider.classList.remove('active');
+// });
 
-slider.addEventListener('mouseup', () => {
-  isDown = false;
-  slider.classList.remove('active');
-});
+// slider.addEventListener('mouseup', () => {
+//   isDown = false;
+//   slider.classList.remove('active');
+// });
 
-slider.addEventListener('mousemove', (e) => {
-  if(!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = x - startX;
-  slider.scrollLeft = scrollLeft - walk;
-  console.log(walk);
-});
+// slider.addEventListener('mousemove', (e) => {
+//   if(!isDown) return;
+//   e.preventDefault();
+//   const x = e.pageX - slider.offsetLeft;
+//   const walk = x - startX;
+//   slider.scrollLeft = scrollLeft - walk;
+//   console.log(walk);
+// });
 
 $(document).ready(function(){
 	$('#nav-icon').click(function(){
