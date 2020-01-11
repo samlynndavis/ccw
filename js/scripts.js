56c96911de7de731390fcf3e93d3e2ce6ece27fb
Samuel Davis <hello@smdvs.com>
2020-01-11 17:05:51 -0600

    Nav bar fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,24 @@
 const runScripts = () => {
+
+    // document.addEventListener("DOMContentLoaded", function(event) {
+ 
+    //     var gallery = document.querySelector('.gallery');
+    //     var flkty = new Flickity( gallery, {
+    //       imagesLoaded: true,
+    //       percentPosition: false,
+    //       cellAlign: 'left',
+    //       pageDots: false,
+    //       wrapAround: true
+    //     } );
+    //     var button = document.querySelector('.button--resize');
+       
+    //     button.addEventListener( 'click', function() {
+    //       gallery.classList.toggle( 'is-expanded' );
+    //       flkty.resize();
+    //     });
+       
+    //    });
+       
     const openAbout = $('#about');
     const closeAbout = $('#close');
 
@@ -62,10 +82,12 @@ const runScripts = () => {
         if (st > lastScrollTop && st > navbarHeight) {
             // Scroll Down
             $('header').removeClass('nav-down').addClass('nav-up');
+            // $('footer').addClass('hide');
         } else {
             // Scroll Up
             if (st + $(window).height() < $(document).height()) {
                 $('header').removeClass('nav-up').addClass('nav-down');
+                // $('footer').removeClass('hide');
             }
         }
     
@@ -74,109 +96,109 @@ const runScripts = () => {
     
     //Drag intro image around screen
     
-    dragElement(document.getElementById("drag-image"));
+    // dragElement(document.getElementById("drag-image"));
     
-    function dragElement(elmnt) {
-        var pos1 = 0,
-            pos2 = 0,
-            pos3 = 0,
-            pos4 = 0;
-        if (document.getElementById(elmnt.id + "header")) {
-            // if present, the header is where you move the DIV from:
-            document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-        } else {
-            // otherwise, move the DIV from anywhere inside the DIV:
-            elmnt.onmousedown = dragMouseDown;
-        }
+    // function dragElement(elmnt) {
+    //     var pos1 = 0,
+    //         pos2 = 0,
+    //         pos3 = 0,
+    //         pos4 = 0;
+    //     if (document.getElementById(elmnt.id + "header")) {
+    //         // if present, the header is where you move the DIV from:
+    //         document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    //     } else {
+    //         // otherwise, move the DIV from anywhere inside the DIV:
+    //         elmnt.onmousedown = dragMouseDown;
+    //     }
     
-        function dragMouseDown(e) {
-            e = e || window.event;
-            e.preventDefault();
-            // get the mouse cursor position at startup:
-            pos3 = e.clientX;
-            pos4 = e.clientY;
-            document.onmouseup = closeDragElement;
-            // call a function whenever the cursor moves:
-            document.onmousemove = elementDrag;
-        }
+    //     function dragMouseDown(e) {
+    //         e = e || window.event;
+    //         e.preventDefault();
+    //         // get the mouse cursor position at startup:
+    //         pos3 = e.clientX;
+    //         pos4 = e.clientY;
+    //         document.onmouseup = closeDragElement;
+    //         // call a function whenever the cursor moves:
+    //         document.onmousemove = elementDrag;
+    //     }
     
-        function elementDrag(e) {
-            e = e || window.event;
-            e.preventDefault();
-            // calculate the new cursor position:
-            pos1 = pos3 - e.clientX;
-            pos2 = pos4 - e.clientY;
-            pos3 = e.clientX;
-            pos4 = e.clientY;
-            // set the element's new position:
-            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-        }
+    //     function elementDrag(e) {
+    //         e = e || window.event;
+    //         e.preventDefault();
+    //         // calculate the new cursor position:
+    //         pos1 = pos3 - e.clientX;
+    //         pos2 = pos4 - e.clientY;
+    //         pos3 = e.clientX;
+    //         pos4 = e.clientY;
+    //         // set the element's new position:
+    //         elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    //         elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    //     }
     
-        function closeDragElement() {
-            // stop moving when mouse button is released:
-            document.onmouseup = null;
-            document.onmousemove = null;
-        }
-    }
+    //     function closeDragElement() {
+    //         // stop moving when mouse button is released:
+    //         document.onmouseup = null;
+    //         document.onmousemove = null;
+    //     }
+    // }
     
-    //toggle 'grab" and 'grabbing' cursors on mousedown and leave
+    // //toggle 'grab" and 'grabbing' cursors on mousedown and leave
     
-    const grabber = document.querySelector('#drag-image');
+    // const grabber = document.querySelector('#drag-image');
     
-    grabber.addEventListener('mousedown', () => {
-        isDown = true;
-        grabber.classList.add('active');
-    });
-    
-    grabber.addEventListener('mouseleave', () => {
-        isDown = false;
-        grabber.classList.remove('active');
-    });
-    
-    grabber.addEventListener('mouseup', () => {
-        isDown = false;
-        grabber.classList.remove('active');
-    });
-    
-    //Roster Slider
-    
-    // const slider = document.querySelector('.roster');
-    // let isDown = false;
-    // let startX;
-    // let scrollLeft;
-    
-    // slider.addEventListener('mousedown', (e) => {
-    //   isDown = true;
-    //   slider.classList.add('active');
-    //   startX = e.pageX - slider.offsetLeft;
-    //   scrollLeft = slider.scrollLeft;
+    // grabber.addEventListener('mousedown', () => {
+    //     isDown = true;
+    //     grabber.classList.add('active');
     // });
     
-    // slider.addEventListener('mouseleave', () => {
-    //   isDown = false;
-    //   slider.classList.remove('active');
+    // grabber.addEventListener('mouseleave', () => {
+    //     isDown = false;
+    //     grabber.classList.remove('active');
     // });
     
-    // slider.addEventListener('mouseup', () => {
-    //   isDown = false;
-    //   slider.classList.remove('active');
+    // grabber.addEventListener('mouseup', () => {
+    //     isDown = false;
+    //     grabber.classList.remove('active');
     // });
     
-    // slider.addEventListener('mousemove', (e) => {
-    //   if(!isDown) return;
-    //   e.preventDefault();
-    //   const x = e.pageX - slider.offsetLeft;
-    //   const walk = x - startX;
-    //   slider.scrollLeft = scrollLeft - walk;
-    //   console.log(walk);
-    // });
+    // //Roster Slider
     
-    // $(document).ready(function(){
-    // 	$('#nav-icon').click(function(){
-    // 		$('#mobile-nav').toggleClass('open');
-    // 	});
-    // });
+    // // const slider = document.querySelector('.roster');
+    // // let isDown = false;
+    // // let startX;
+    // // let scrollLeft;
+    
+    // // slider.addEventListener('mousedown', (e) => {
+    // //   isDown = true;
+    // //   slider.classList.add('active');
+    // //   startX = e.pageX - slider.offsetLeft;
+    // //   scrollLeft = slider.scrollLeft;
+    // // });
+    
+    // // slider.addEventListener('mouseleave', () => {
+    // //   isDown = false;
+    // //   slider.classList.remove('active');
+    // // });
+    
+    // // slider.addEventListener('mouseup', () => {
+    // //   isDown = false;
+    // //   slider.classList.remove('active');
+    // // });
+    
+    // // slider.addEventListener('mousemove', (e) => {
+    // //   if(!isDown) return;
+    // //   e.preventDefault();
+    // //   const x = e.pageX - slider.offsetLeft;
+    // //   const walk = x - startX;
+    // //   slider.scrollLeft = scrollLeft - walk;
+    // //   console.log(walk);
+    // // });
+    
+    // // $(document).ready(function(){
+    // // 	$('#nav-icon').click(function(){
+    // // 		$('#mobile-nav').toggleClass('open');
+    // // 	});
+    // // });
     
     
     // init controller
@@ -186,7 +208,7 @@ const runScripts = () => {
     $('.fade-up').each(function () {
     
         // build a tween
-        var fadeTween = TweenMax.from($(this), 1.5, {opacity: "0", ease: Power4.easeInout});
+        var fadeTween = TweenMax.from($(this), 1.2, {opacity: "0", ease: Power4.easeInout});
     
         // build a scene
         var scene = new ScrollMagic.Scene({
@@ -197,24 +219,24 @@ const runScripts = () => {
     
     });
     
-    var rosterTween = new TweenMax.from('#roster', 1, {
-        opacity: "0",
-        ease: Power4.easeInOut
-    });
-    
-    // var overviewTween = new TweenMax.from('#overview', 2, {
+    // var rosterTween = new TweenMax.from('#roster', 1, {
     //     opacity: "0",
-    //     y: "20%",
-    //     ease: Power4.easeOut,
-    //     delay: 0.2
+    //     ease: Power4.easeInOut
     // });
     
-    var containerScene = new ScrollMagic.Scene({
-            triggerElement: '#roster'
-        })
-        .setTween(rosterTween)
-        // .addIndicators()
-        .addTo(controller);
+    // // var overviewTween = new TweenMax.from('#overview', 2, {
+    // //     opacity: "0",
+    // //     y: "20%",
+    // //     ease: Power4.easeOut,
+    // //     delay: 0.2
+    // // });
+    
+    // var containerScene = new ScrollMagic.Scene({
+    //         triggerElement: '#roster'
+    //     })
+    //     .setTween(rosterTween)
+    //     // .addIndicators()
+    //     .addTo(controller);
     
     // var introScene = new ScrollMagic.Scene({
     //         triggerElement: '#intro'
@@ -244,13 +266,13 @@ const runScripts = () => {
     timelineMobile
         .to(menu, 1, { y: "0%", ease: Power4.easeInOut});
     
-    $(document).on('click', ".hamburger", function () {
+    $(openMenu).on('click', function () {
       $(menu).toggleClass('on');
       $("body").addClass('noscroll');
       timelineMobile.reversed() ? timelineMobile.play() : timelineMobile.reverse();
     });
       
-    $(document).on('click', ".close", function () {
+    $(closeMenu).on('click', function () {
         timelineMobile.reversed() ? timelineMobile.play() : timelineMobile.reverse();
     });
     
@@ -268,8 +290,103 @@ const runScripts = () => {
 
     // var Scrollbar = window.Scrollbar;
     // Scrollbar.init(document.querySelector('#my-scrollbar'));
+
+    
+    //Get the button:
+mybutton = document.getElementById("toTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    mybutton.style.visibility = "visible";
+    mybutton.style.opacity = "1";
+  } else {
+    mybutton.style.visibility = "hidden";
+    mybutton.style.opacity = "0";
+  }
+};
+
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", function () {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
 }
+
 runScripts();
+
+$(document).ready(function() {
+    if($('body').hasClass('home')){
+      rosterFade();
+    }
+  });
+
+const rosterFade = () => {
+
+    var controller = new ScrollMagic.Controller();
+
+    var rosterTween = new TweenMax.from('#roster', 1, {
+        opacity: "0",
+        ease: Power4.easeInOut
+    });
+    
+    // var overviewTween = new TweenMax.from('#overview', 2, {
+    //     opacity: "0",
+    //     y: "20%",
+    //     ease: Power4.easeOut,
+    //     delay: 0.2
+    // });
+    
+    var containerScene = new ScrollMagic.Scene({
+            triggerElement: '#roster'
+        })
+        .setTween(rosterTween)
+        // .addIndicators()
+        .addTo(controller);
+    
+    // var introScene = new ScrollMagic.Scene({
+    //         triggerElement: '#intro'
+    //     })
+    //     .setTween(overviewTween)
+    //     .addIndicators()
+    //     .addTo(controller);
+}
+
+const footerArrow = document.body.classList.contains("home");
+
+document.getElementById('toTop').className+= " rest";
+
+
+// This function will run a throttled script every 300 ms
+var checkFooter = _.throttle(() => {
+    // Run JavaScript stuff here
+    console.log('checkFooter');
+
+    // Detect scroll position
+    let scrollPosition = Math.round(window.scrollY);
+
+    // If we've scrolled 100px, add "visible" class to the header
+    if (scrollPosition > 500) {
+        document.querySelector('.footer').classList.add('hide');
+    }
+    // If not, remove "sticky" class from header
+    else {
+        document.querySelector('.footer').classList.remove('hide');
+    }
+}, 300);
+
+// Run the checkHeader function every time you scroll
+window.addEventListener('scroll', checkFooter);
+
+var moveTicker = _.throttle(() => {
+    console.log
+})
+
 
 // barba.init({
 //     transitions: [{
@@ -337,4 +454,3 @@ runScripts();
 //     views: [],
 //     debug: true
 // });
-
